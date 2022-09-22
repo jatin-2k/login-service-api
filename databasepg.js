d8d9bf3838d7c9a8ebdf242b42a9bc11c65390f1
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-09-22 13:53:44 +0530

    setting up postgres

---
+++ databasepg.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+require('dotenv').config({ path: require('path').resolve(__dirname, './.env') });
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || process.env.DATABASE_URL_DEV,
+    ssl: process.env.DATABASE_URL ? true : false
+});
+
+module.exports = pool;
+
+// pool.query('select * from test_table')
+//     .then((res) => {
+//         console.log(`RESULT:`);
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(`ERROR:`);
+//         console.log(err);
+//     })
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var pool = require('../databasepg');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.query('SELECT * from test_table')
+  .then((RESULT) => {
+    res.send(RESULT.rows);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(502).send();
+  });
 });
 
 module.exports = router;
